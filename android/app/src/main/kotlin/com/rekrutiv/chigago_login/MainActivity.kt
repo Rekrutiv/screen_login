293adad098c047a0c514ac4b9b93e4a18f31c30b
VOLODYMYR <rekrutivvolodymyr@gmail.com>
2021-03-03 19:38:51 +0200

    git initial animatr container

---
+++ android/app/src/main/kotlin/com/rekrutiv/chigago_login/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rekrutiv.chigago_login
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:chigago_login/screen/login_signup.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(LoginSignupUI());
+}
+
+class LoginSignupUI extends StatelessWidget {
+  const LoginSignupUI({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Login Signup UI",
+      home: LoginSignupScreen(),
+    );
+  }
+}
