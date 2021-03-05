411cb75b8231a98b48023f60407da4c04eb4d52a
VOLODYMYR <rekrutivvolodymyr@gmail.com>
2021-03-05 15:50:03 +0200

    add bloc validation

--- lib/screen/login_signup.dart
+++ lib/screen/login_signup.dart
@@ -1,5 +1,7 @@
 import 'package:chigago_login/bloc_form/my_form_bloc.dart';
 import 'package:chigago_login/config/palette.dart';
+import 'package:chigago_login/screen/widget/EmailInput.dart';
+import 'package:chigago_login/screen/widget/PasswordInput.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -248,70 +250,8 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
   }
 }
 
-class EmailInput extends StatelessWidget {
-  const EmailInput({Key key, this.focusNode}) : super(key: key);
 
-  final FocusNode focusNode;
 
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<MyFormBloc, MyFormState>(
-      builder: (context, state) {
-        return TextFormField(
-          initialValue: state.email.value,
-          focusNode: focusNode,
-          decoration: InputDecoration(
-            icon: const Icon(Icons.email),
-            labelText: 'Email',
-            helperText: 'A complete, valid email e.g. joe@gmail.com',
-            errorText: state.email.invalid
-                ? 'Please ensure the email entered is valid'
-                : null,
-          ),
-          keyboardType: TextInputType.emailAddress,
-          onChanged: (value) {
-            context.read<MyFormBloc>().add(EmailChanged(email: value));
-          },
-          textInputAction: TextInputAction.next,
-        );
-      },
-    );
-  }
-}
-
-class PasswordInput extends StatelessWidget {
-  const PasswordInput({Key key, this.focusNode}) : super(key: key);
-
-  final FocusNode focusNode;
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<MyFormBloc, MyFormState>(
-      builder: (context, state) {
-        return TextFormField(
-          initialValue: state.password.value,
-          focusNode: focusNode,
-          decoration: InputDecoration(
-            icon: const Icon(Icons.lock),
-            helperText:
-                '''Password should be at least 8 characters with at least one letter and number''',
-            helperMaxLines: 2,
-            labelText: 'Password',
-            errorMaxLines: 2,
-            errorText: state.password.invalid
-                ? '''Password must be at least 8 characters and contain at least one letter and number'''
-                : null,
-          ),
-          obscureText: true,
-          onChanged: (value) {
-            context.read<MyFormBloc>().add(PasswordChanged(password: value));
-          },
-          textInputAction: TextInputAction.done,
-        );
-      },
-    );
-  }
-}
 
 class SuccessDialog extends StatelessWidget {
   @override
